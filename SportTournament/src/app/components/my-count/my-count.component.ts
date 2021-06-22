cca8302660e19994087512415761049b6c604536
Randall Garcia <rquinteros-2019166@kinal.edu.gt>
2021-06-22 17:19:31 -0600

    Se creo el perfil del usuario

--- SportTournament/src/app/components/my-count/my-count.component.ts
+++ SportTournament/src/app/components/my-count/my-count.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-my-count',
   templateUrl: './my-count.component.html',
-  styleUrls: ['./my-count.component.css']
+  styleUrls: ['./my-count.component.css'],
+  providers: [UserService]
 })
 export class MyCountComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public _userService: UserService,
+  ) { }
 
   ngOnInit(): void {
   }
